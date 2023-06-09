c42dcce72e0c37f56a3370076c5da81b6bede89b
Purice Victor <pvictor0110@gmail.com>
2023-06-09 12:39:33 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import './styles.less'; 
 
 function App() {
   return (
-    <div>
-      <h1>Calculla React App</h1>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-num">0</div>
+        <div className="current-num">1</div>
+        </div> 
+        <button>AC</button>
+        <button>DEL</button>
+        <button>%</button>
+        <button>÷</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>×</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>-</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button className="column-two">0</button>
+        <button>,</button>
+        <button>=</button>
+      
     </div>
   );
 }
