bbf1e1dec58715af8101f38bf5723ccb44d405fe
Purice Victor <pvictor0110@gmail.com>
2023-06-22 14:17:51 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import { useReducer } from 'react';
 
 
 const ACTIONS = {
-
+  ADD_DIGIT: 'add-digit', 
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear', 
+  DELETE_DIGIT: 'delete-digit', 
+  EVALUATE: 'evaluate',
 }
 function reducer(state, {type,payload}) {
 
